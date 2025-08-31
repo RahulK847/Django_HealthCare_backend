d7487a2fe7b893d5d37bd95c2bcf9b2dded823c8
Rahul Kumar <84038547+RahulK847@users.noreply.github.com>
2025-08-31 19:58:00 +0530

    Update settings.py

--- healthcare_backend/healthcare_backend/settings.py
+++ healthcare_backend/healthcare_backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)p2867eq=6^+#0=+y2@59e_xrrni51u2!xxj8k%qcl0f6@7cpn'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-)p2867eq=6^+#0=+y2@59e_xrrni51u2!xxj8k%qcl0f6@7cpn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'healthcare-backend-4bbd.onrender.com', '.onrender.com']
 
 
 # Application definition
