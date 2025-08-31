9996d0ded8023cc54345d3b0d3fff2441c1f149d
Rahul Kumar <84038547+RahulK847@users.noreply.github.com>
2025-08-31 23:31:42 +0530

    Update urls.py

--- healthcare_backend/users/urls.py
+++ healthcare_backend/users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserRegistrationView
 from .token_views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='user_login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
